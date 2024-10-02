56dadaffd4c852854170cae7ef2cb727691c9380
Aditya Pawar <aadipawar4940@gmail.com>
2024-10-02 21:33:59 +0530

    hardcoded be url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.BASE_URL,
+    baseURL: "http://65.2.149.146:3000/api",
     withCredentials: true, // Enable sending cookies with requests
 });
 
